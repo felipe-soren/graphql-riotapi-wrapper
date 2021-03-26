cb339e4219773977e348c503baae618cd25ab1bb
Felipe Novaes <felipe.novaes@kroton.com.br>
2021-03-26 12:11:53 -0300

    Adicionando retorno de historico de partidas no objeto summoner

--- src/datasources/summoner.js
+++ src/datasources/summoner.js
@@ -15,15 +15,21 @@ class RiotSummonerAPI extends RESTDataSource {
     return await this.get(`/league/v4/entries/by-summoner/${summonerId}`);
   }
 
+  async _getSummonerHistory(summonerId) {
+    return await this.get(`match/v4/matchlists/by-account/${summonerId}?endIndex=10`)
+  }
+
   async getSummoner(name) {
     if (!name) throw new Error("Valid sumonerName not provided");
 
     const userData = await this.get(`/summoner/v4/summoners/by-name/${name}`);
     const leagueData = await this._getSummonerLeague(userData.id);
+    const {matches} = await this._getSummonerHistory(userData.accountId)
 
     return {
       ...userData,
       leagues: leagueData,
+      history: matches,
     };
   }
 }
